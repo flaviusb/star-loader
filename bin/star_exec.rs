90280bb597afec9d25029a6f6a54d93a55151754
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2025-01-17 20:57:30 +1300

    Noodling.

--- bin/star_exec.rs
+++ bin/star_exec.rs
@@ -2,3 +2,51 @@ use star_loader::*;
 
 fn main() {
 }
+
+/*
+
+extern crate syscalls;
+use syscalls::{Sysno, syscall};
+
+#[cfg(not(any(
+    target_arch = "x86_64",
+    //target_arch = "aarch64",
+    //target_arch = "riscv64",
+)))]
+compile_error!("unsupport arch");
+
+cfg_if::cfg_if! {
+  if #[cfg(target_arch = "x86_64")] {
+    todo!();
+  } else if #[cfg(target_arch = "riscv64")]{
+    compile_error!("unsupport arch");
+  } else if #[cfg(target_arch="aarch64")]{
+    compile_error!("unsupport arch");
+  }
+}
+
+
+
+
+match unsafe { syscall!(Sysno::clone3, ... ) } {
+  Ok(0) => {
+    // Child process
+    // Now we need to trampoline into the loaded stuff
+    asm!{
+    };
+    syscall1(Sysno::exit, 0usize).unwrap();
+    unreachable!()
+  }
+  Ok(pid) => {
+    // Parent process
+    syscall1(Sysno::exit, 0usize).unwrap();
+    unreachable!()
+  }
+  Err(err) => {
+    eprintln!("clone() failed: {}", err);
+    syscall1(Sysno::exit, 1usize).unwrap();
+    unreachable!()
+  }
+}
+
+ */
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,26 @@
 /*
 
+
+Binary File ⇆ Rust Representation of the Unprocessed Thing ⇆ Plain text representation ⇄ Process image state
+
+Everything that 'runs and affects lib state as such' eg fixusp, initialisers, dependencies etc etc needs to have a way to mark that it has been run already, so that the Process Image State can be represented faithfully by the other forms of the thing, and also so that you can eg 'rebuild deps', 'introspect fixups', etc etc.
+
+They can also be purged, for example when compiling you might want to purge that stuff to a) save space b) Maybe make it faster? c) make it harder to de-monolithicise d) hide non-public APIs etc so that people don't depend on unkept invariants etc
+
+Compile time
+Load-time
+Run-time
+
+Run initialisers
+Patchups
+
+
+Processing of the Unprocessed Thing
+☑ Compile time linking (← maybe we have a thing to do this for compilers... ???)
+☐ Load-time linking
+→ ☐ Run initialisers...
+☐ Run-time linking
+
 pub fn load
 
 pub fn star_exec
