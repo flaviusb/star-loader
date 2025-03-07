c929de1535b2bcc3cc055e6725f16d7eb6248a6c
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2025-03-08 12:47:53 +1300

    Lots of noodling.
    Just thoughts still for now.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,21 @@
 
 Binary File ⇆ Rust Representation of the Unprocessed Thing ⇆ Plain text representation ⇄ Process image state
 
-Everything that 'runs and affects lib state as such' eg fixusp, initialisers, dependencies etc etc needs to have a way to mark that it has been run already, so that the Process Image State can be represented faithfully by the other forms of the thing, and also so that you can eg 'rebuild deps', 'introspect fixups', etc etc.
+Everything that 'runs and affects lib state as such' eg fixups, initialisers, dependencies etc etc needs to have a way to mark that it has been run already, so that the Process Image State can be represented faithfully by the other forms of the thing, and also so that you can eg 'rebuild deps', 'introspect fixups', etc etc.
 
 They can also be purged, for example when compiling you might want to purge that stuff to a) save space b) Maybe make it faster? c) make it harder to de-monolithicise d) hide non-public APIs etc so that people don't depend on unkept invariants etc
+
+Exec
+Constants
+
+
+The Rust thing is a bunch of datastructures with invariants
+Eg
+
+let sectionorder: Vec<usize>;
+
+
+
 
 Compile time
 Load-time
@@ -13,6 +25,10 @@ Run-time
 
 Run initialisers
 Patchups
+'Name' lookup / insertion / removal / changing (plus relation to fixups) → kv store-like with name (or more complicated things) as query, and value possibilities: binary values, 'addresses', fn/coro/etc thing (address + stack discipline / etc)
+Dependency lookup
+
+layout stuff - so you can eg interleave writable and non-writable (shareable) pages in order to be able to do fixups etc without fucking shareability up 
 
 
 Processing of the Unprocessed Thing
